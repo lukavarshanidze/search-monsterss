52a162a92e25e392cb57ed43562ef9b4e08e361a
lukkys <lvarshanidze1@gmail.com>
2022-12-23 17:28:31 +0400

    first commit

---
+++ src/components/search-box/search.box.component.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+import './search-box.styles.css';
+
+const SearchBox = ({className, placeholder, onChangeHandler}) => (
+    
+
+            <input 
+                type='search'
+                className={`search-box ${className}`} 
+                placeholder={placeholder}
+                onChange={onChangeHandler}/>
+)
+    
+
+
+export default SearchBox;
